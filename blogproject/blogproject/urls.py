ad496cc2244d1d53429ccddffd2108bdbc4fc49b
jin419111582 <419111582@qq.com>
2020-12-15 20:26:56 +0800

    'createproject'

--- blogproject/blogproject/urls.py
+++ blogproject/blogproject/urls.py
@@ -13,9 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from PIL.Image import logger
 from django.contrib import admin
 from django.urls import path
 
+#1.导入系统的logging
+
+import logging
+
+
+#2.创建（获取）日志器
+
+logging=logging.getLogger('django')
+
+
+from django.http import HttpResponse
+
+
+def log(request):
+
+    # 3.使用日志器记录信息
+    logger.info('info')
+    return HttpResponse('test')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', log),
 ]
